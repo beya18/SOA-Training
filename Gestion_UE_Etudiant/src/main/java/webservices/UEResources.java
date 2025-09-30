d44ca9550d3881d07e2b3aca0bd398b86e3f9a9c
beya18 <131160592+beya18@users.noreply.github.com>
2025-09-30 21:10:03 +0100

    workshop 1

---
+++ Gestion_UE_Etudiant/src/main/java/webservices/UEResources.java
@@ -0,0 +1,44 @@
+package webservices;
+
+import metiers.UniteEnseignementBusiness;
+
+import javax.ws.rs.Path ;
+
+@Path("/ue") //URI
+public class UEResources {
+
+    UniteEnseignementBusiness helper = new UniteEnseignementBusiness();
+    @Path("/liste")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response liste()
+    {
+        return Response.status(200).entity(
+                helper.getListeUE()
+        ).build() ;
+    }
+
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response ajout(UnitEnseignement ue)
+    {
+        if(helper.addUniteEnseignement(ue))
+        { return Response.status(201).etity("added successfully")}
+        else {return Response.status(409).entity("already exist")}
+    }
+
+
+
+
+
+    @Path("/hello") //
+    @GET
+    @Produces(Mediatype.TEXT_PLAIN)
+    public Response sayHELLO(){
+        return Response
+                .status(200)
+                .entity ("Hello 4infini")
+                .build();
+
+    }
+}
