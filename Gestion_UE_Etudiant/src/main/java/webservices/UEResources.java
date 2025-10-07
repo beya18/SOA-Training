a4d4cf6e616619d3381d308439b5da6809612265
beya18 <131160592+beya18@users.noreply.github.com>
2025-10-07 15:16:58 +0100

    workshop 2

--- Gestion_UE_Etudiant/src/main/java/webservices/UEResources.java
+++ Gestion_UE_Etudiant/src/main/java/webservices/UEResources.java
@@ -19,17 +19,44 @@ public class UEResources {
     }
 
 
-    @Path("/ajout")
+    @Path("/add")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response ajout(UniteEnseignement ue)
+    public Response addUe(UniteEnseignement ue)
     {
         if(helper.addUniteEnseignement(ue))
         { return Response.status(201).etity("added successfully")}
         else {return Response.status(409).entity("already exist")}
     }
 
+
+    //delete
+    @Path("/delete/{code}")
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteUE(@PathParam("code") int code){
+        if(helper.addUniteEnseignement(code)) {
+            return Response
+                    .status(200)
+                    .entity("deleted")
+                    .build();
+        } return Response
+                .status(404)
+                .entity("not found")
+                .build();
+    }
+
+    //search by code
+    @Path("/search")
+    @GET
+    @Produces(MediaType.APPLUCATION_JSON)
+    public Response search(QueryParam(value = "s") int semestre){
+        return Response
+                .status(200)
+                .entity(this.helper.getUEBySemestre(semestre))
+                .build();
+    }
 
 
 
